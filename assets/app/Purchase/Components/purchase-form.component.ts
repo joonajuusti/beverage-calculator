fee21d63efa675d2eaeb95e0ad00c6761badade0
Joona Juusti <joona.juusti@gmail.com>
2018-03-29 14:43:30 +0300

    Update purchase-form

--- assets/app/Purchase/Components/purchase-form.component.ts
+++ assets/app/Purchase/Components/purchase-form.component.ts
@@ -1,16 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from '@angular/forms';
 
 import { Purchase } from "../Models/purchase";
 import { PurchaseService } from '../Services/purchase.service';
+import { StoreService } from '../Services/store.service';
 
 @Component({
   selector: 'purchase-form',
   templateUrl: './purchase-form.component.html',
+  styles:[`
+    .button-margin{
+      margin-left: 15px
+    }
+  `]
 })
-export class PurchaseFormComponent {
-  constructor(private purchaseService: PurchaseService) {
+export class PurchaseFormComponent implements OnInit {
+  constructor(
+    private purchaseService: PurchaseService,
+    private storeService: StoreService) {}
 
+  stores: string[] = [];
+  purchases: Purchase[] = [];
+
+  ngOnInit(){
+    this.loadSpecies()
+  }
+
+  loadSpecies() {
+    this.storeService.getStores().subscribe(
+      stores => this.stores = stores,
+      err => {
+        console.log(err);
+      });
   }
 
   submitPurchase(form: NgForm) {
@@ -21,8 +42,8 @@ export class PurchaseFormComponent {
       form.value.pepsiPriceInPurchase,
       form.value.monsterAmountInCans,
       form.value.monsterPricePerCan);
-    this.purchaseService.addPurchase(purchase)
-      .subscribe(
+
+    this.purchaseService.addPurchase(purchase).subscribe(
         data => console.log(data),
         error => console.error(error)
       );
@@ -32,4 +53,7 @@ export class PurchaseFormComponent {
   clear(form: NgForm) {
     form.resetForm();
   }
+
+
+
 }
